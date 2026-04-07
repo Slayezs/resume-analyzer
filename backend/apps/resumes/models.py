ce55e0c333da1efe180ecf17169e142eed473eb1
Chirag <chirag657krsingh@gmail.com>
2026-04-07 12:42:37 +0530

    Added Resume model for file storage

--- backend/apps/resumes/models.py
+++ backend/apps/resumes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Resume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='resumes/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Resume {self.id} - {self.user}"
