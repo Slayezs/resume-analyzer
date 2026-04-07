7226d855062295811159314b0069a4fe81e5b216
Chirag <chirag657krsingh@gmail.com>
2026-04-07 19:38:00 +0530

    Implemented rule-based skill extraction logic

---
+++ backend/apps/resumes/services/extractor.py
@@ -0,0 +1,11 @@
+from .skills_data import SKILLS
+
+def extract_skills(text):
+    text = text.lower()
+    found_skills = []
+
+    for skill in SKILLS:
+        if skill in text:
+            found_skills.append(skill)
+
+    return list(set(found_skills))
