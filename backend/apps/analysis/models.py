1e0dfc3eea992e9c89dccaad19e6249288cf4b95
Chirag <chirag657krsingh@gmail.com>
2026-04-07 19:57:49 +0530

    Created AnalysisResult model to store matching results

--- backend/apps/analysis/models.py
+++ backend/apps/analysis/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from apps.resumes.models import Resume
 
-# Create your models here.
+class AnalysisResult(models.Model):
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    job_description = models.TextField()
+
+    match_score = models.FloatField()
+    ats_score = models.FloatField()
+
+    matched_skills = models.JSONField(default=list)
+    missing_skills = models.JSONField(default=list)
+
+    created_at = models.DateTimeField(auto_now_add=True)
